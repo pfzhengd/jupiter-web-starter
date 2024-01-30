d4a523f754f5a4897a008f32e0fe471454fee976
郑鹏飞 <zhengpengfei@elephx.io>
2024-01-31 05:04:59 +0800

    init: 初始化

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello NUI
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'Index',
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  beforeDestroy() {},
+}
+</script>
+
+<style lang="scss">
+@import './index.scss';
+</style>
